5e691cea8a3e201d87ba85c4b5cd62b1e0e6c537
xoghks79 <xoghks79@gmail.com>
2025-05-11 14:29:03 +0900

    테스트 환이 v1

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, STS4!";
+        return "Hello, STS4! LTH";
     }
 }
